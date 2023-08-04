2277ee42b21ffe84dc403907d39915bcb95f3d3a
Eray Chumak <eray_chumak@protonmail.com>
2023-08-04 01:30:51 +0100

    Add styles for main content

--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,8 @@ import DecoratedPFP from "@/components/DecoratedPFP"
 import styles from "./page.module.css"
 import Image from "next/image"
 
+import { JBM_Font, Roboto_Font } from "../fonts";
+
 export default function Home() {
   return (
     <>
@@ -32,17 +34,32 @@ export default function Home() {
         </nav>
       </header>
       <main>
-        <section>
-          <article>
-            <h1>Hi, I&apos;m Eray <span id="phonetics">(pronounced Air-I)</span></h1>
-            <p id="position">Frontend Developer</p>
-            <p id="desc">A curious human being with an eye for detail and a passion for design and writing software programs.</p>
-            <button>Let&apos;s work together</button>
-          </article>
-          <article>
+        <section className={styles.landingBanner}>
+          <article className={styles.pfpContainer}>
             <DecoratedPFP/>
           </article>
+          <article className={styles.intro}>
+            <h1>Hi, I&apos;m Eray</h1>
+            <p className={styles.phonetics}>(pronounced Air-I)</p>
+            <p className={`${Roboto_Font.className} ${styles.position}`}>
+              Frontend Developer
+            </p>
+            <p className={Roboto_Font.className}>A curious human being with an eye for detail and a passion for design and writing software programs.</p>
+            <button className={JBM_Font.className}>Let&apos;s work together</button>
+          </article>
         </section>
+        <section className={styles.wipContainer}>
+        </section>
+        <footer className={styles.footer}>
+          <div className={styles.wip}>
+            <p>Excuse the mess ^_^. This site is work in progress! 🏗️🧑‍💻</p>
+          </div>
+          <p>
+            This site is open-source, view the source-code
+            on my GitHub <a href="https://github.com/eraychumak/portfolio" target="_blank" rel="noopener noreferrer">repository</a>
+          </p>
+          <p>Copyright &copy; 2023 Eray Chumak</p>
+        </footer>
       </main>
     </>
   )
