16aec0277eb7327e4a87d82689cb0c893be6975e
Eray Chumak <eray_chumak@protonmail.com>
2023-08-07 00:19:40 +0100

    Add google search logo feature

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import "./globals.css";
 
+import Script from "next/script";
 import { JBM_Font } from "../fonts";
 
 export const metadata = {
@@ -7,9 +8,17 @@ export const metadata = {
   description: "One-stop shop to learn more about me and get in touch.",
 };
 
+const logoFeature = {
+  "@context": "https://eraychumak.dev",
+  "@type": "Organization",
+  "url": "https://eraychumak.dev",
+  "logo": "https://eraychumak.dev/logo.svg"
+}
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <Script id="LdJsonItemLogoFeature" type="application/ld+json" dangerouslySetInnerHTML={JSON.stringify(logoFeature)}/>
       <body className={JBM_Font.className}>{children}</body>
     </html>
   )
