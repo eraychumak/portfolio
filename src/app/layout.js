63b15758556a688b83d41127b7503f8b814a535a
Eray Chumak <eray_chumak@protonmail.com>
2023-08-07 01:33:27 +0100

    Add more metadata

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,24 +1,43 @@
 import "./globals.css";
 
-import Script from "next/script";
 import { JBM_Font } from "../fonts";
+import Script from "next/script";
+
+const structuredData = {
+  "@context": "https://schema.org/",
+  "@type": "Person",
+  "email": "mailto:eraychumak_@outlook.com",
+  "image": "https://eraychumak.dev/pfp.jpg",
+  "jobTitle": "Frontend Developer",
+  "gender": "male",
+  "name": "Eray Chumak",
+  "url": "https://eraychumak.dev",
+  "founder": [{
+    "legalName": "Web Line Community Interest Company",
+    "nonprofitStatus": "LimitedByGuaranteeCharity",
+    "slogan": "You innovate it. We code it. You price it. You own it."
+  }],
+  "logo": "https://eraychumak.dev/logo.svg"
+}
+
 
 export const metadata = {
   title: "Eray Chumak - Portfolio",
   description: "One-stop shop to learn more about me and get in touch.",
+  themeColor: "#1b1f28",
+  keywords: ["Frontend", "Developer", "Software", "Eray", "Chumak", "Engineer", "Programmer"],
 };
-
-const logoFeature = {
-  "@context": "https://eraychumak.dev",
-  "@type": "Organization",
-  "url": "https://eraychumak.dev",
-  "logo": "https://eraychumak.dev/logo.svg"
-}
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <Script id="LdJsonItemLogoFeature" type="application/ld+json" dangerouslySetInnerHTML={JSON.stringify(logoFeature)}/>
+      <head>
+        <Script
+          strategy="beforeInteractive"
+          id="my-ldjson-data"
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{ __html: JSON.stringify(structuredData) }}/>
+      </head>
       <body className={JBM_Font.className}>{children}</body>
     </html>
   )
