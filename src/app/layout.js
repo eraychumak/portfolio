b4ee7e6c35283e12336411611c9af19b29a99630
Eray Chumak <eray_chumak@protonmail.com>
2023-08-07 01:58:45 +0100

    Fix invalid schema

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,21 +5,25 @@ import Script from "next/script";
 
 const structuredData = {
   "@context": "https://schema.org/",
-  "@type": "Person",
+  "@type": "ProfessionalService",
+  "currenciesAccepted": "GBP",
+  "priceRange": "$$",
   "email": "mailto:eraychumak_@outlook.com",
   "image": "https://eraychumak.dev/pfp.jpg",
   "jobTitle": "Frontend Developer",
   "gender": "male",
   "name": "Eray Chumak",
   "url": "https://eraychumak.dev",
+  "telephone": "+447305682150",
+  "address": "United Kingdom",
+  "slogan": "Just your friendly neighborhood frontend developer student",
   "founder": [{
+    "@type": "Organization",
     "legalName": "Web Line Community Interest Company",
     "nonprofitStatus": "LimitedByGuaranteeCharity",
     "slogan": "You innovate it. We code it. You price it. You own it."
-  }],
-  "logo": "https://eraychumak.dev/logo.svg"
+  }]
 }
-
 
 export const metadata = {
   title: "Eray Chumak - Portfolio",
