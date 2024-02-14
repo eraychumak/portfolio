301d03ded2440780861ffa4c925eaaf47eba08a0
Eray Chumak <eray_chumak@protonmail.com>
2024-02-14 02:22:47 +0000

    Add new Footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import styles from "@/components/Footer.module.css";
+
+export default function Footer() {
+  return (
+    <section className={styles.container}>
+      <p>
+        This site is open-source, view the source-code
+        in my GitHub <a href="https://github.com/eraychumak/portfolio" target="_blank" rel="noopener noreferrer">repository</a>.
+      </p>
+      <p>Copyright &copy; 2024 Eray Chumak</p>
+    </section>
+  );
+}
