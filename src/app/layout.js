30c83c3d779ef182bd1930cb3f17d1aa3f88798e
Eray Chumak <eray_chumak@protonmail.com>
2024-02-14 02:22:27 +0000

    Add new fonts, remove old ones

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,20 +1,56 @@
-import "./globals.css";
+import { Heebo, Libre_Baskerville, Padauk, Source_Code_Pro } from "next/font/google";
 
-import { JBM_Font } from "../fonts";
 import SEO from "@/components/SEO";
+import Sidebar from "@/components/Sidebar";
+import Footer from "@/components/Footer";
+
+import "./globals.css";
 
 export const metadata = {
   title: "Eray Chumak - Portfolio",
-  description: "One-stop shop to learn more about me and get in touch.",
-  themeColor: "#1b1f28",
+  description: "Frontend developer living in North West, England. Visit my portfolio website to learn more about my experience and get in touch.",
   keywords: ["Frontend", "Developer", "Software", "Eray", "Chumak", "Engineer", "Programmer"],
 };
 
+export const viewport = {
+  themeColor: "#1b1f28",
+}
+
+const fontHeebo = Heebo({
+  subsets: ["latin"],
+  variable: "--font-heebo",
+  display: "swap"
+});
+
+const fontLibreBaskerville = Libre_Baskerville({
+  subsets: ["latin"],
+  variable: "--font-libre_baskerville",
+  display: "swap",
+  weight: ["400", "700"]
+});
+
+const fontPadauk = Padauk({
+  subsets: ["latin"],
+  variable: "--font-padauk",
+  display: "swap",
+  weight: ["400", "700"]
+});
+
+const fontSourceCodePro = Source_Code_Pro({
+  subsets: ["latin"],
+  variable: "--font-sourcecode-pro",
+  display: "swap",
+});
+
 export default function RootLayout({ children }) {
+  const fonts = `${fontHeebo.variable} ${fontLibreBaskerville.variable} ${fontPadauk.variable} ${fontSourceCodePro.variable}`;
+
   return (
-    <html lang="en">
-      <body className={JBM_Font.className}>
+    <html lang="en" className={fonts}>
+      <body>
+        <Sidebar/>
         {children}
+        <Footer/>
         <SEO/>
       </body>
     </html>
