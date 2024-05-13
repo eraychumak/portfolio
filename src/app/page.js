74653a6b2d6c61b454708f13d9672a998ff7a979
Eray Chumak <eraychumak_@outlook.com>
2024-05-14 00:58:43 +0100

    Refresh layout

--- src/app/page.js
+++ src/app/page.js
@@ -1,25 +1,14 @@
 import Image from "next/image";
+import ChatBubbleBottomCenterIcon from "@heroicons/react/24/outline/ChatBubbleBottomCenterIcon";
+
+import Timeline from "./_components/Timeline";
 
 import styles from "./page.module.css";
 
 export default function Home() {
   return (
     <main className={styles.container}>
-      <h1>Eray Chumak</h1>
-      <section className={styles.intro}>
-        <article className={styles.desc}>
-          <p>
-            Hi, I&apos;m <span className={styles.pronounce} title="Pronounced Air-i">Eray</span>, a &lt;<span className={styles.highlight1}>frontend developer</span>&gt; living
-            in North West, England. I like working by myself or in a
-            team, building useful &lt;<span className={styles.highlight2}>apps for the web</span>&gt;.
-          </p>
-          <p>
-            I am a curious individual with
-            not two but four eyes, which is
-            advantageous for that extra mile
-            towards detail.
-          </p>
-        </article>
+      <header className={styles.header}>
         <article className={styles.pfp}>
           <Image
             className={styles.pfp}
@@ -30,10 +19,25 @@ export default function Home() {
             priority
           />
         </article>
+        <h1>Eray Chumak</h1>
+        <p>BSc (Hons) Web Design & Development</p>
+      </header>
+      <section className={styles.intro}>
+        <p className={styles.desc}>
+          Hi, I&apos;m <span className={styles.pronounce} title="Pronounced Air-i">Eray</span>, a &lt;<span className={styles.highlight1}>frontend developer</span>&gt; living
+          in North West, England. I like working by myself or in a
+          team, building useful &lt;<span className={styles.highlight2}>apps for the web</span>&gt;.
+          I am a curious individual with
+          not two but four eyes, which is
+          advantageous for that extra mile
+          towards detail.
+        </p>
+        <section className={styles.quote}>
+          <ChatBubbleBottomCenterIcon width="2rem"/>
+          <p>I follow a <i>&apos;You can learn anything if you put enough time and know how to search&apos;</i> approach.</p>
+        </section>
       </section>
-      <div className={styles.wip}>
-        <p>Some things may not function ^_^. This site is work in progress! 🏗️🧑‍💻</p>
-      </div>
+      <Timeline/>
     </main>
   )
 }
