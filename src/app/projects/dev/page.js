dd15b4972ce8eecf472b45dfc1f72d1ce8d933e0
Eray Chumak <eraychumak_@outlook.com>
2024-05-25 18:39:05 +0100

    Add dev projects page

---
+++ src/app/projects/dev/page.js
@@ -0,0 +1,27 @@
+import Link from "next/link";
+import Image from "next/image";
+
+import styles from "./page.module.css";
+import ProjectBadge from "../_components/ProjectBadge";
+
+export default function PageProjectsDev() {
+  return (
+    <main className={styles.container}>
+      <header className={styles.header}>
+        <h1>Dev. Projects</h1>
+      </header>
+      <section>
+        <h2>2023</h2>
+        <section className={styles.projects}>
+          <Link href="/projects/dev/mtfm" className={styles.project}>
+            <Image src="/assets/projects/mtfm/preview.png" width="1903" height="1065" alt=""/>
+            <div className={styles.content}>
+              <p>Mark This For Me</p>
+              <ProjectBadge label="Personal Project"/>
+            </div>
+          </Link>
+        </section>
+      </section>
+    </main>
+  )
+}
