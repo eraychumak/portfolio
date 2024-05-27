3813055918918f9df67ddcd47e435eeb1bf434ba
Eray Chumak <eraychumak_@outlook.com>
2024-05-27 08:45:16 +0100

    Allow colour variations

--- src/app/projects/_components/ProjectBadge.js
+++ src/app/projects/_components/ProjectBadge.js
@@ -1,9 +1,19 @@
 import styles from "./ProjectBadge.module.css";
 
 export default function ProjectBadge({ label }) {
+  let bg = styles.default;
+
+  if (label === "Personal Project") {
+    bg = styles.personalProject;
+  }
+
+  if (label === "Client Project") {
+    bg = styles.client;
+  }
+
   return (
     <section className={styles.container}>
-      <p className={styles.badge}>{label}</p>
+      <p className={`${styles.badge} ${bg}`} data-type={label}>{label}</p>
     </section>
   );
 }
