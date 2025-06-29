8268664447f3b7eea7aa015c9ee8f6873f334e6a
Eray Chumak <eraychumak_@outlook.com>
2025-06-29 01:45:30 +0100

    feat: update sitemap

--- src/app/sitemap.js
+++ src/app/sitemap.js
@@ -3,6 +3,26 @@ export default function sitemap() {
     {
       url: "https://eraychumak.dev",
       lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 1,
+    },
+    {
+      url: "https://eraychumak.dev/projects/dev",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
+    {
+      url: "https://eraychumak.dev/projects/dev/dmiu",
+      lastModified: new Date(),
+      changeFrequency: "yearly",
+      priority: 0.6,
+    },
+    {
+      url: "https://eraychumak.dev/projects/dev/mtfm",
+      lastModified: new Date(),
+      changeFrequency: "yearly",
+      priority: 0.6,
     },
   ]
 }
