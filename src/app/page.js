408dc4d091cc3a7825d3a4bccba3d14fe9744e29
Eray Chumak <eraychumak_@outlook.com>
2026-01-03 03:35:08 +0000

    feat: refine copy and layout

--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,8 @@ import Image from "next/image";
 import ChatBubbleBottomCenterIcon from "@heroicons/react/24/outline/ChatBubbleBottomCenterIcon";
 
 import Timeline from "../_components/Timeline";
+import DisplayNameImage from "@/assets/display_name_white.webp";
+import PfpImage from "@/assets/pfp.webp";
 
 import styles from "./page.module.css";
 
@@ -9,42 +11,43 @@ export default function Home() {
   return (
     <main className={styles.container}>
       <header className={styles.header}>
-        <article className={styles.pfp}>
+        <section className={styles.quickInfo}>
           <Image
-            className={styles.pfp}
-            src="/pfp.avif"
-            alt="Eray sitting on an escalator"
-            width={3072}
-            height={4080}
+            src={DisplayNameImage}
+            className={styles.displayName}
+            alt="Eray Koschell"
+            role="heading"
             priority
-            unoptimized
           />
-        </article>
-        <h1>Eray Koschell</h1>
-        <p className={styles.jobTitle}>Software Engineer</p>
-        <p className={styles.degree}>BSc (Hons) Web Design & Development</p>
+          <p className={styles.jobTitle}>Software Engineer</p>
+          <p className={styles.degree}>BSc (Hons) Web Design & Development</p>
+        </section>
+        <section className={styles.pfp}>
+          <Image
+            src={PfpImage}
+            alt="A portrait picture of Eray with Manchester city skyline as the background."
+            priority
+          />
+        </section>
       </header>
       <section className={styles.intro}>
         <p className={styles.desc}>
-          Hi, I&apos;m <span className={styles.pronounce} title="Pronounced Air-i">Eray</span>, a &lt;<span className={styles.highlight1}>software engineer</span>&gt; based
-          in North West England. I enjoy working both independently and collaboratively,
-          building useful &lt;<span className={styles.highlight2}>web applications</span>&gt; that solve real problems.
-          I&apos;m a curious individual with
-          not two but four eyes, giving me
-          an advantage when it comes to
-          attention to detail.
+          Hi, I&apos;m <span className={styles.pronounce} title="Pronounced Air-i">Eray</span>, a &lt;<span className={styles.highlight1}>Software Engineer</span>&gt; based
+          in Manchester. I work independently and collaboratively,
+          building high-quality products and working with the great teams behind them.
         </p>
         <section className={styles.quote}>
           <ChatBubbleBottomCenterIcon width="2rem"/>
-          <p>I follow a <i>&apos;You can learn anything with enough time and the right approach to research&apos;</i> philosophy.</p>
+          <p>I believe great products come from teams who care about the craft, the people they build with, and the impact they create. The right culture brings out our best work.</p>
         </section>
       </section>
-      <hr/>
       <Timeline/>
-      <hr/>
       <section className={styles.shallWe}>
         <h2>Shall we?</h2>
-        <p>I&apos;m currently open to frontend design and development opportunities, as well as freelancing and client work. I&apos;m available for office-based, hybrid, or remote positions <i>(Full/Part Time)</i>.</p>
+        <p>Use the sidebar to get in touch with me.</p>
+        <h6>Location preference</h6>
+        <p>I find I work best in a hybrid office setting with a few days in the office and a few days working from home.</p>
+        <p>However, I&apos;m open to fully remote work depending on the role, team, and mission.</p>
       </section>
     </main>
   )
